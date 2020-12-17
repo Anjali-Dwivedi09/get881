79559e8733bf3211add6cebb19fc167d44ea3c52
Anjali-Dwivedi09 <76148933+Anjali-Dwivedi09@users.noreply.github.com>
2020-12-17 13:45:40 +0530

    added diff

--- Calculator.java
+++ Calculator.java
@@ -2,8 +2,11 @@ Class Calculator{
   public static int add(int n, int m){
     return (n+m);
   }
-  
+  public static int diff(int n, int m){
+  return(n-m);
+  }
   public static void main(String[] args){
     System.out.println(add(10,20));
+    System.out.println(diff(10,20));
   }
 }
